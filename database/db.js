8f8eb4d203eeb020b98160b7583b713554bd4cb1
Amodh <n.amodh@agathsya.net>
2024-03-21 17:15:28 +0530

    azure connect to db

--- database/db.js
+++ database/db.js
@@ -1,13 +1,12 @@
 const mysql = require('mysql2');
-var fs = require('fs');
+
 
 const db = mysql.createConnection({
-  host: 'dexter.mysql.database.azure.com',
-  user: 'Amodh',
-  password: 'Dexter@2132',
-  database: 'task_pulse',
-  port:"3306",
- 
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port:process.env.DB_PORT,
 });
 
 db.connect((err) => {
